5441f56d17456ec54d8322d080e0e77267dada95
Matheus Silva <matheusamaral.silva@aleyant.com>
2025-02-27 12:42:44 -0300

    api fix

--- src/services/transaction.service.js
+++ src/services/transaction.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import authHeader from './auth-header'
 
-const API_URL = 'https://salus-682523916a3b.herokuapp.com//api/'
+const API_URL = 'https://salus-682523916a3b.herokuapp.com/api/'
 
 class UserService {
   getTransactions(params) {
